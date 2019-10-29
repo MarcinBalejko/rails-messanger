684d3f68f5908102a89af0cd5642b59d591adf18
MarcinBalejko <mbbm9@o2.pl>
2019-10-29 19:30:41 +0100

    :white_check_mark: new + create actions and their routes, new root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'messages#index'
   
-  get '/messges' => 'messages#index'
+  get 'messages' => 'messages#index'
+
+  get 'messages/new' => 'messages#new'
+
+  post 'messages' => 'messages#create'
 
 end
